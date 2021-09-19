d674b96cfc46d0ea41b015aa8245f294be762e60
MdAbdulKayumKhan <kayum261@gmail.com>
2021-09-19 13:49:00 +0600

    similar look differen data

--- src/App.js
+++ src/App.js
@@ -2,37 +2,79 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const singer ={
-    name: 'Eva Rahman',
-    job: 'singer'
-  }
-  const singerStyle = {
-    backgroundColor: 'red',
-    color: 'white',
-    padding: '20px'
-  }
+  const nayoks =['Rubel', 'Bapparaz', 'Koober', 'Bappi', 'shuvo'];
+  const cinemas = [
+    {nayok: 'koober', nayika: 'kopila'},
+    {nayok: 'Rubel', nayika: 'moushumi'},
+    {nayok: 'Razzak', nayika: 'shabana'}
+
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-      {/* JSX stands for JavaScript XML */}
-      <div className="container">
-        <p style ={singerStyle}>{singer.name +' ' + singer.job}</p>
-      </div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{color:'green', background: 'cyan'}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          nayoks.map(nayok => <li>{nayok}</li>)
+        }
+      </ul>
+      
+      {
+        cinemas.map(cinema => <Cinema nayok={cinema.nayok} nayika={cinema.nayika}></Cinema>)
+      }
+     
+      
     </div>
   );
 }
 
+function Person(props){
+  console.log(props);
+  const personStyle = {
+    backgroundColor: 'cyan',
+    padding: '10px',
+    color: 'white',
+    margin: '10px',
+    borderRadius: '20px',
+    border: '2px solid lightblue'
+  }
+  return (
+    <div 
+    // using class, personStyle-object and derict object
+    // className="person"
+    // style={personStyle}
+    style={{background: 'red', borderRadius: '20px', margin: '20px'}}
+    > 
+      <h2>Name: {props.name}</h2>
+      <p>His heroin: {props.nayika}</p>
+    </div>
+  )
+  
+}
+
+function Cinema(props){
+
+  return (
+    <div  className="person">
+      <h2>Nayko: {props.nayok}</h2>
+      <h4>Nayika: {props.nayika}</h4>
+    </div>
+  )
+}
+
+function Friend(){
+  return (
+    <div>
+      <h3>Phone: xp45</h3>
+    </div>
+  )
+}
+
+{/* <Person name="Ruble" nayika="moushumi"> </Person>
+      <Person name="salman khan" nayika="catrina"> </Person> */}
+
+/*
+       <Person name={nayoks[0]} nayika="moushumi"> </Person>
+      <Person name={nayoks[1]} nayika="catrina"> </Person>
+      <Person name={nayoks[2]} nayika="catrina"> </Person>
+
+*/ 
 export default App;
